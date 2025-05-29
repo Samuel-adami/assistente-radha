2aeb1634123456ae92b0ad07bc0b8ec18be5ddfd
Samuel-adami <samueladami84@gmail.com>
2025-05-29 14:12:43 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import axios from 'axios';
-
-// ✅ Configura interceptor para enviar automaticamente o header Authorization
-axios.interceptors.request.use((config) => {
-  const credenciais = localStorage.getItem("credenciais"); // Exemplo: "usuario:senha"
-  if (credenciais) {
-    config.headers.Authorization = credenciais;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
 
 serviceWorkerRegistration.register();
 
