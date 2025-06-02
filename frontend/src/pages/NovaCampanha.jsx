b17f652a29855aa412206650fdfbe4d09dd2f0ce
Samuel-adami <samueladami84@gmail.com>
2025-06-02 11:03:20 -0300

    Update NovaCampanha.jsx

--- frontend/src/pages/NovaCampanha.jsx
+++ frontend/src/pages/NovaCampanha.jsx
@@ -106,7 +106,7 @@ function NovaCampanha() {
 
       <button
         onClick={enviar}
-        className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700"
+        className="bg-red-800 text-white px-4 py-2 rounded hover:bg-purple-700"
       >
         Criar Campanha
       </button>
